d8d6ee8ac5a3de854271e85ebaa7c7c183007d32
Mfinst <Spammfach@web.de>
2020-01-15 18:26:25 +0100

    fixed a bug where markAuthors was not checking of abstracts were found

--- R/corpus_gen.R
+++ R/corpus_gen.R
@@ -1,18 +1,18 @@
 corpus_gen <-
 function(data.vector, lang, furtherStops = NULL) {
   # erstellt den Corpus aus einer Vektorquelle
-  corpus = VCorpus(VectorSource(as.vector(data.vector)), readerControl = list(language=lang))
-  
+  corpus = tm::VCorpus(tm::VectorSource(as.vector(data.vector)), readerControl = list(language=lang))
+
   # Verarbeitungsschritte mit tm_map
-  corpus = tm_map(corpus, content_transformer(tolower))
-  corpus = tm_map(corpus, stripWhitespace)
-  corpus = tm_map(corpus, removePunctuation) 
-  corpus = tm_map(corpus, stemDocument, lang)
-  corpus = tm_map(corpus, removeWords, stopwords(lang))
-  
-  # Wenn weitere Filterwörter vorhanden sind
+  corpus = tm::tm_map(corpus, tm::content_transformer(base::tolower))
+  corpus = tm::tm_map(corpus, base::stripWhitespace)
+  corpus = tm::tm_map(corpus, tm::removePunctuation)
+  corpus = tm::tm_map(corpus, tm::stemDocument, lang)
+  corpus = tm::tm_map(corpus, tm::removeWords, stopwords(lang))
+
+  # Wenn weitere Filterw?rter vorhanden sind
   if(!is.null(furtherStops)) {
-    corpus = tm_map(corpus, removeWords, furtherStops)
+    corpus = tm::tm_map(corpus, tm::removeWords, furtherStops)
   }
   return(corpus)
 }
--- functions/markAuthors.R
+++ functions/markAuthors.R
@@ -12,11 +12,11 @@ markAuthors <- function(ddResult, mappedValues) {
   # Initialisierung
   # Type Konvertierung wegen potenzieller Faktormengen
   ddResult = type.convert(x = ddResult, as.is = TRUE)
-  
+
   # Whitespace Entfernung an Anfang und Ende der DOIs
   ddResult$A_DOI = trimws(ddResult$A_DOI)
   ddResult$B_DOI = trimws(ddResult$B_DOI)
-  
+
   # Erstelle Dataframe für bessere Lesbarkeit der Funktion
   mappedValues = as.data.frame(mappedValues, stringsAsFactors = FALSE)
   colnames(mappedValues)[1] = "GROUPS"
@@ -26,22 +26,22 @@ markAuthors <- function(ddResult, mappedValues) {
   # Entferne trailing Whitespace für Autor und DOIs
   mappedValues$AUTHOR = trimws(mappedValues$AUTHOR)
   mappedValues$DOI = trimws(mappedValues$DOI)
-  
+
   # Main Loop; über alle Autoren der cocit Tabelle
   for(i in 1:length(ddResult$A)) {
-    
+
     row = ddResult[i,] # holt die aktuelle Zeile
-    
+
     # Zuweisung der Zeilen Informationen
     authorA = row$A
     authorADOI = row$A_DOI
     authorB = row$B
     authorBDOI = row$B_DOI
-    
+
     # prüfe ob die DOI in den exportierten Daten exisitiert
     abs1 = as.character(mappedValues$ABSTRACT[as.character(mappedValues$DOI)==authorADOI])
     abs2 = as.character(mappedValues$ABSTRACT[as.character(mappedValues$DOI)==authorBDOI])
-    
+
     # Wenn ein Abstract mehrfach existiert, selektiere das erste
     if(length(abs1)>1) {
       abs1 = abs1[1]
@@ -49,19 +49,24 @@ markAuthors <- function(ddResult, mappedValues) {
     if(length(abs2)>1) {
       abs2 = abs2[1]
     }
-    
+
     # Wenn ein Abstract gefunden wurde
     # Markiere Autor
-    if (abs1 != "") {
-      authorA = paste("*", authorA, sep = "")
-      print(paste('-A-', authorA, sep = ' '))
-      ddResult[i,]$A = as.character(authorA)
+    if (length(abs1 != 0)) {
+      if (abs1 != "") {
+        authorA = paste("*", authorA, sep = "")
+        print(paste('-A-', authorA, sep = ' '))
+        ddResult[i,]$A = as.character(authorA)
+      }
     }
-    if (abs2 != "") {
-      authorB = paste("*", authorB, sep = "")
-      print(paste('-B-', authorB, sep = ' '))
-      ddResult[i,]$B = as.character(authorB)
+    if (length(abs2) != 0) {
+      if (abs2 != "") {
+        authorB = paste("*", authorB, sep = "")
+        print(paste('-B-', authorB, sep = ' '))
+        ddResult[i,]$B = as.character(authorB)
+      }
     }
+
   }
   return(ddResult)
 }
